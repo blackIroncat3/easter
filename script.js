625ec44e4450c299ab28e30a65f36bba8eb6717e
blackIroncat3 <76999487+blackIroncat3@users.noreply.github.com>
2022-04-10 22:41:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 const dateEl = document.getElementById("date");
-const date1 = "Date: 2022.04.17.";
+let data = [
+    {year: 2022, date: `04.17`},
+    {year: 2023, date: `04.09`},
+    {year: 2024, date: `03.31`},
+    {year: 2025, date: `04.20`},
+    {year: 2026, date: `04.20`},
+    {year: 2027, date: `03.28`},
+    {year: 2028, date: `04.16`},
+    {year: 2029, date: `04.01`},
+    {year: 2030, date: `04.21`},
+];
 async function timer(){
     const date = new Date()
-    const date2 = new Date("2022.04.17. 00:00")
+    let y = Date.parse(date) > Date.parse(`${new Date().getFullYear()}.${data.find(d=>d.year === new Date().getFullYear()).date} 00:00`) ? new Date().getFullYear()+1 : new Date().getFullYear(); 
+    let reader = data.find(d=>d.year === y)
+    const date2 = new Date(`${y}.${reader.date}. 00:00`)
     const ss = (date2-date)/1000;
     const days = Math.floor(ss / 3600 / 24);
     const hours = Math.floor(ss/3600)%24;
@@ -16,6 +28,7 @@ async function timer(){
     hoursEl.innerHTML= formaTime(hours);
     minsEl.innerHTML= formaTime(mins);
     secondsEl.innerHTML= formaTime(sec);
+    let date1 = `Date: ${y}.${reader.date}.`;
     dateEl.innerHTML=date1
 }
 function formaTime(time){
